482c124a953d637d31242e41191ebf71965cade1
Assaf Neufeld <assaf.neu@gmail.com>
2022-02-04 17:43:41 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var list = new List<int>();
+for (int i = 0; i < 100; i++)
+{
+    list.Add(i);
+}
+
+list = list.Where(i => i % 21 == 1).ToList();
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
